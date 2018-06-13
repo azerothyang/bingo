bb5b38855e2bbc13f7cca14f313892b361eb7591
azerothYang <azerothyangg@gmail.com>
2018-06-13 10:08:24 +0800

    remove some comments

--- src/main.go
+++ src/main.go
@@ -16,52 +16,6 @@ func setupRouter() *gin.Engine {
 	// gin.DisableConsoleColor()
 	r := gin.Default()
 	r.Use(token.HandleToken(), holdup.CheckHold(), ckcache.CheckCache()) //中间件
-	// Ping test
-	//r.GET("/ping", func(c *gin.Context) {
-	//	c.JSON(http.StatusOK, gin.H{
-	//		"code": 200,
-	//		"msg": "hello",
-	//		"data": "hello world",
-	//	})
-	//})
-	//
-	//// Get user value
-	//r.GET("/user/:name", func(c *gin.Context) {
-	//	user := c.Params.ByName("name")
-	//	value, ok := DB[user]
-	//	if ok {
-	//		c.JSON(200, gin.H{"user": user, "value": value})
-	//	} else {
-	//		c.JSON(200, gin.H{"user": user, "status": "no value"})
-	//	}
-	//})
-	//
-	//Authorized group (uses gin.BasicAuth() middleware)
-	// Same than:
-	//authorized := r.Group("/")
-	//authorized.Use(gin.BasicAuth(gin.Credentials{
-	//       "foo":  "bar",
-	//       "manu": "123",
-	//}))
-	//authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
-	//	"foo":  "bar", // user:foo password:bar
-	//	"manu": "123", // user:manu password:123
-	//}))
-
-	//authorized.POST("admin", func(c *gin.Context) {
-	//	user := c.MustGet(gin.AuthUserKey).(string)
-	//
-	//	// Parse JSON
-	//	var json struct {
-	//		Value string `json:"value" binding:"required"`
-	//	}
-	//
-	//	if c.Bind(&json) == nil {
-	//		DB[user] = json.Value
-	//		c.JSON(200, gin.H{"status": "ok"})
-	//	}
-	//})
-
 	//增加testController 路由
 	router.AddTestControllerRoute(r)
 
