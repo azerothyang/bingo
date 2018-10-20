43e4603c31b9fa2c61a101c74002c7de79dd64c5
azerothYang <azerothyangg@gmail.com>
2018-10-21 00:37:19 +0800

    加入unixNano随机种子

--- src/util/random.go
+++ src/util/random.go
@@ -1,11 +1,15 @@
 package util
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 
 //生成随机字符串
 func RandomStr(length int) string {
+	rand.Seed(time.Now().UnixNano())
 	runes := make([]rune, length)
 	for i := 0; i < length; i++ {
 		runes[i] = letters[rand.Intn(len(letters))]
